dd236ac8c96ee40189941d1643ab841e07a36ac1
varun1452 <varungaudani80@gmail.com>
2023-08-26 17:57:02 +0530

    calculator interface

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,9 +1,38 @@
-import React from "react"
+import React from "react";
+import {Button} from 'react-bootstrap';
 
 function calculator() {
-return(
-     <div> Calculator </div>
+  return (
+    <div className="Calculator">
+      <div className="Calculator-wrapper">
+        <div className="Calculator-Heading">
+          <h3 className="disable">Calculator in react</h3>
+        </div>
+        <div className="Calculator-screen">
+          <div className="Calculator-output">
+            <span>
 
-)
+            </span>
+          </div>
+          <div className="Calculator-Backspace">
+            <button className="Backspace-Btn">
+              <i className=""> </i>
+            </button>
+            <span>
+
+            </span>
+          </div>
+          <div className="Calculator_Button"> 
+                <Button type='button' className='Top-Btn' value='AC'>
+                  AC
+                </Button>
+                <button type="button" className='Top-Btn' value=''>
+                  <sup>+</sup>/<sub>-</sub>
+                </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default calculator;
