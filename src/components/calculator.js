c0266d6d877da9021b96d158932f27f1f8dba4a7
varun1452 <varungaudani80@gmail.com>
2023-09-01 17:13:41 +0530

    AC , BACKSPACE and PLUS/MINUS button done

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,14 +1,45 @@
-import React, {useState} from "react";
-import {Button} from 'react-bootstrap';
-import {FaBackspace} from 'react-icons/fa'
+import React, { useState } from "react";
+import { Button } from 'react-bootstrap';
+import { FaBackspace } from 'react-icons/fa'
 
 function Calculator() {
-  const [input, setInput]= useState ("");
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
   const handleInput = (e) => {
     const value = e.target.value;
 
-    setInput((input)=> input + value );
+    setInput((input) => input + value);
   }
+
+  const handleArithematicOperator = (e) => {
+    const value = e.target.value;
+
+    if (value == "=") {
+      if (input) {
+        setOutput(eval(input));
+        setInput("");
+      }
+    } else if (value == "AC") {
+      setInput("");
+      setOutput(0)
+    }
+    else if (value == "pm")
+      if (input) {
+        // eslint-disable-next-line no-eval
+        let Check = eval(input);
+        setInput(Check !== 0 ? (Check > 0 ? `-${Check}` : `${Math.abs(Check)}`) : "");
+      }
+
+  }
+
+
+  const handleBackspace = () => {
+    if (input.length > 0) {
+      setInput((input) => input.slice(0, -1));
+    }
+  }
+
+
   return (
     <div className="Calculator">
       <div className="Calculator-Wrapper">
@@ -18,91 +49,91 @@ function Calculator() {
         <div className="Calculator-screen Aligned">
           <div className="Calculator-output">
             <span>
-            
+
             </span>
           </div>
         </div>
-          <div className="Calculator-Input Aligned">
-            <span>
-            {input?  input : "0"}
-            </span>
-          </div>
-          <div className="Calculator-Button"> 
-                <Button type='button' className='Top-Btn' value='AC'>
-                  AC
-                </Button>
-                <Button type="button" className='Top-Btn' value='+/-'>
-                +/-
-                </Button>
-                
-                <Button type="button" className='Top-Btn' value='%'>
-                %
-                </Button>
-               
-                <Button type="button" className='Special-Btn' value='backspace'>
-                <FaBackspace/>
-                </Button>
+        <div className="Calculator-Input Aligned">
+          <span>
+            {input ? input : "0"}
+          </span>
+        </div>
+        <div className="Calculator-Button">
+          <Button type='button' className='Top-Btn' value='AC' onClick={handleArithematicOperator}>
+            AC
+          </Button>
+          <Button type="button" className='Top-Btn' value='pm' onClick={handleArithematicOperator} >
+            +/-
+          </Button>
 
-                <Button type='button' className='Normal-Btn' value='7' onClick={handleInput}>
-                  7
-                </Button>
-                <Button type="button" className='Normal-Btn' value='8' onClick={handleInput}>
-                  8
-                </Button>
+          <Button type="button" className='Top-Btn' value='%'>
+            %
+          </Button>
 
-                <Button type="button" className='Normal-Btn' value='9' onClick={handleInput}>
-                  9
-                </Button>
-                
-                <Button type="button" className='Special-Btn' value='X'>
-                X
-                </Button>
-               
-                <Button type="button" className='Normal-Btn' value='4' onClick={handleInput}>
-                4
-                </Button>
+          <Button type="button" className='Special-Btn' value='backspace' onClick={handleBackspace}>
+            <FaBackspace />
+          </Button>
 
-                <Button type='button' className='Normal-Btn' value='5' onClick={handleInput}>
-                  5
-                </Button>
-                <Button type="button" className='Normal-Btn' value='6' onClick={handleInput}>
-                  6
-                </Button>
-                  
-                <Button type="button" className='Special-Btn' value='÷'>
-                  ÷
-                </Button>
-               
-                <Button type="button" className='Normal-Btn' value='1' onClick={handleInput}>
-                  1
-                </Button>
+          <Button type='button' className='Normal-Btn' value='7' onClick={handleInput}>
+            7
+          </Button>
+          <Button type="button" className='Normal-Btn' value='8' onClick={handleInput}>
+            8
+          </Button>
 
-                <Button type='button' className='Normal-Btn' value='2' onClick={handleInput}>
-                  2
-                </Button>
-                <Button type="button" className='Normal-Btn' value='3' onClick={handleInput}>
-                  3
-                </Button>
-                
-                <Button type="button" className='Special-Btn' value='-'>
-                -
-                </Button>
-               
-                <Button type="button" className='Normal-Btn' value='.'>
-               .
-                </Button>
+          <Button type="button" className='Normal-Btn' value='9' onClick={handleInput}>
+            9
+          </Button>
 
-                <Button type="button" className='Normal-Btn' value='0' onClick={handleInput}>
-               0
-                </Button>
+          <Button type="button" className='Special-Btn' value='X'>
+            X
+          </Button>
 
-                <Button type='button' className='Special-Btn' value='+'>
-                  +
-                </Button>
-                <Button type="button" className='Special-Btn' value='='>
-                =
-                </Button>
-          </div>
+          <Button type="button" className='Normal-Btn' value='4' onClick={handleInput}>
+            4
+          </Button>
+
+          <Button type='button' className='Normal-Btn' value='5' onClick={handleInput}>
+            5
+          </Button>
+          <Button type="button" className='Normal-Btn' value='6' onClick={handleInput}>
+            6
+          </Button>
+
+          <Button type="button" className='Special-Btn' value='÷'>
+            ÷
+          </Button>
+
+          <Button type="button" className='Normal-Btn' value='1' onClick={handleInput}>
+            1
+          </Button>
+
+          <Button type='button' className='Normal-Btn' value='2' onClick={handleInput}>
+            2
+          </Button>
+          <Button type="button" className='Normal-Btn' value='3' onClick={handleInput}>
+            3
+          </Button>
+
+          <Button type="button" className='Special-Btn' value='-'>
+            -
+          </Button>
+
+          <Button type="button" className='Normal-Btn' value='.'>
+            .
+          </Button>
+
+          <Button type="button" className='Normal-Btn' value='0' onClick={handleInput}>
+            0
+          </Button>
+
+          <Button type='button' className='Special-Btn' value='+'>
+            +
+          </Button>
+          <Button type="button" className='Special-Btn' value='='>
+            =
+          </Button>
+        </div>
       </div>
     </div>
   );
